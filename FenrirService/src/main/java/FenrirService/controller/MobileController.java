b4a165c91db53b1febb068581b881e9c498a5d5b
Robert-Jan Eduard Koot <heyokajan@msn.com>
2015-05-18 21:05:17 +0200

    Mobile controller update to include request mappings and assocaited funtions

--- FenrirService/src/main/java/FenrirService/controller/MobileController.java
+++ FenrirService/src/main/java/FenrirService/controller/MobileController.java
@@ -1,5 +1,21 @@
 package FenrirService.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import FenrirService.representation.mobile.*;
+
+@RestController
 public class MobileController {
+	
+	@RequestMapping("/mobile/data")
+	public Data data(){
+		return new Data();
+	}
+	
+	@RequestMapping("/mobile/auth")
+	public Auth auth(){
+		return new Auth();
+	}
 
 }
