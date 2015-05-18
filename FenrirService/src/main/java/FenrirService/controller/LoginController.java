04151d0070a26cf03ed744fa7ca2fa06cf6e2021
Robert-Jan Eduard Koot <heyokajan@msn.com>
2015-05-18 19:32:49 +0200

    Comments added to login controller and associated classes

--- FenrirService/src/main/java/FenrirService/controller/LoginController.java
+++ FenrirService/src/main/java/FenrirService/controller/LoginController.java
@@ -6,20 +6,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import FenrirService.representation.login.Auth;
 
+/*
+ * ====================================================================================
+ * LoginController is used to manage anything related to the Fenrir Login API.
+ * It will create the required request mappings and execute the backend functionalities
+ * required to make a successful authorization attempt.
+ * ====================================================================================
+ */
+
 @RestController
 public class LoginController {
 	
+	// url: <domain>/FenrirService/login?username=<user>&company=<company>
 	@RequestMapping("/login")
 	public Auth auth(@RequestParam(value="username", defaultValue="None") String username, @RequestParam(value="company", defaultValue="None") String company){
 		
+		//state variable used in data object to declare return message
 		int state = 0;
 		
+		//If either username or company uses the default values declared by requestParam return 1 else execute authorization attempt.
 		if(username.equals("None") || company.equals("None")){
 			state = 1;
 		}else{
 			state = 2;
 		}
 		
+		//Create Auth data object to return message
 		return new Auth(state);
 	}
 
--- FenrirService/src/main/java/FenrirService/representation/login/Auth.java
+++ FenrirService/src/main/java/FenrirService/representation/login/Auth.java
@@ -1,11 +1,24 @@
 package FenrirService.representation.login;
 
+/*
+ * ====================================================================================
+ * Auth data object required by REST for returning the proper information. This data
+ * object is used for return messages for an authorization attempt by Fenrir Login API
+ * ====================================================================================
+ */
+
 public class Auth {
 	
+	// message and state variable to be returned to the Fenrir Login API
 	private final String message;
+	private final int state;
 	
+	// Creation of data object requires state. State declares informational message to be sent back.
 	public Auth(int state){
 		
+		this.state = state;
+		
+		//switch declaring content of message
 		switch (state) {
 		case 0:
 			message = "Error occured in LoginController";
@@ -20,6 +33,10 @@ public class Auth {
 			message = "";
 			break;
 		}
+	}
+	
+	public int getState(){
+		return state;
 	}
 	
 	public String getMessage(){
