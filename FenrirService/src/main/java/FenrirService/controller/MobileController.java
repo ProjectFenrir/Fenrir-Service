a01be5adc1c9043ebf738ebfe430dd9943a0e480
Robert-Jan Eduard Koot <heyokajan@msn.com>
2015-05-20 11:16:55 +0200

    Check to mobile added for chekcing if mobile has to do something

--- FenrirService/src/main/java/FenrirService/controller/MobileController.java
+++ FenrirService/src/main/java/FenrirService/controller/MobileController.java
@@ -1,6 +1,7 @@
 package FenrirService.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import FenrirService.representation.mobile.*;
@@ -16,6 +17,21 @@ public class MobileController {
 	@RequestMapping("/mobile/auth")
 	public Auth auth(){
 		return new Auth();
+	}
+	
+	//Check for new login attempts <url>/FenrirService/mobile/check?number=<number>
+	@RequestMapping("/mobile/check")
+	public Check check(@RequestParam(value="number", defaultValue="0") String number){
+		
+		//check if number is not equal to 0
+		if(!number.equals("0")){
+			//code for checking with database for new login attempts
+			
+			return new Check(1);
+		}else{
+			return new Check(0);
+		}
+		
 	}
 
 }
---
+++ FenrirService/src/main/java/FenrirService/representation/mobile/Check.java
@@ -0,0 +1,15 @@
+package FenrirService.representation.mobile;
+
+public class Check {
+
+	final private int returnInt; 
+	
+	public Check(int returnInt){
+		this.returnInt = returnInt;
+	}
+	
+	public int getRI(){
+		return returnInt;
+	}
+
+}
